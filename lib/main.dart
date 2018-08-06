50b333d88611cf3ebe523bff3f0d81a34c9f0e18
colorful300 <xiaoyouziv@163.com>
2018-08-06 21:19:04 +0800

    删除注释

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ void main() {
 }
 
 class AppMain extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
