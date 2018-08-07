a2bc08b0e6a02c5e70a79ff3b6de3f7c175a85ba
colorful300 <xiaoyouziv@163.com>
2018-08-07 22:54:09 +0800

    修改包名和应用名

--- android/app/src/main/java/com/knowngeo/knowngeo/MainActivity.java
+++ android/app/src/main/java/com/knowngeo/knowngeo/MainActivity.java
@@ -1,8 +1,9 @@
-package com.example.knowngeo;
+package com.knowngeo.knowngeo;
 
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+
 
 public class MainActivity extends FlutterActivity {
   @Override
